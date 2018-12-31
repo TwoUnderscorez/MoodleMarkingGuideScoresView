34a1d6ba99406b1a213633ea070ee89478e5cc78
TwoUnderscorez <ron.yutkin@gmail.com>
2018-12-31 17:32:52 +0200

    declare a few base functions

---
+++ config - example.py
@@ -0,0 +1,9 @@
+""" Config file, overrides config.pkl
+"""
+
+# UPDATE VALUES AND RENAME TO config.ph
+MYSQL_HOST = ''
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
+MYSQL_DB = ''
+MYSQL_PORT = 3306
--- moodledb.py
+++ moodledb.py
@@ -1,17 +1,15 @@
 """ Connection to the Moodle database
 """
-import pymysql
-from pymysql.connections import Connection as PySQLCon
+import pymysql as sql
+from pymysql.connections import Connection
+from pymysql.cursors import Cursor
 
 
 class MoodleDB():
     """ Connection to the Moodle database
     """
 
-    def __init__(self, user: str,
-                 password: str,
-                 host: str,
-                 **kwargs: dict):
+    def __init__(self, user: str, password: str, host: str, **kwargs):
         """ Initialize a connection to the moodle database
 
         Arguments:
@@ -23,9 +21,27 @@ class MoodleDB():
             port {int} -- MySQL server port
             database {str} -- database name
         """
-        self.conn: PySQLCon = pymysql.connect(
+        self.conn: Connection = sql.connect(
             user=user, host=host, password=password, **kwargs)
-        self.cur = self.conn.cursor()
+        self.cur: Cursor = self.conn.cursor()
+
+    def get_course_list(self):
+        pass
+
+    def get_assignments_list(self, courseid: int):
+        pass
+
+    def get_user_info(self, userid: int) -> dict:
+        """ Get the user's info, name, surname and email
+
+        Arguments:
+            userid {int} -- The user's ID in to moodle database
+
+        Returns:
+            dict -- a dictionary containing `id`, `name`, `surname` 
+                    and `email` fields
+        """
+        return {}
 
     def get_instanceid_to_username_map(self):
         pass
