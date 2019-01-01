4b12f383214ae5fffb2ca10213edccd3fc84a716
TwoUnderscorez <ron.yutkin@gmail.com>
2019-01-01 22:23:33 +0200

    implemented get_user_info and start implementing _get_fileds_from_table

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import config
 def main():
 
     db = moodledb.MoodleDB(config.MYSQL_USER, config.MYSQL_PASSWORD,
-                           config.MYSQL_HOST, db=config.MYSQL_DB, port=config.MYSQL_PORT)
-    print(db)
+                           config.MYSQL_HOST, config.MYSQL_DB, port=config.MYSQL_PORT)
+    ron = db.get_user_info(3)
+    print(ron)
+    db.close_no_save()
 
 
 if __name__ == "__main__":
--- moodledb.py
+++ moodledb.py
@@ -9,18 +9,19 @@ class MoodleDB():
     """ Connection to the Moodle database
     """
 
-    def __init__(self, user: str, password: str, host: str, **kwargs):
+    def __init__(self, user: str, password: str, host: str, db: str, **kwargs):
         """ Initialize a connection to the moodle database
 
         Arguments:
             user {str} -- MySQL username
             password {str} -- MySQL password
             host {str} -- MySQL server ip/domain
+            db {str} -- database name
 
-        Keyword Arguments:
+        Optional Keyword Arguments:
             port {int} -- MySQL server port
-            database {str} -- database name
         """
+        self.db = db
         self.conn: Connection = sql.connect(
             user=user, host=host, password=password, **kwargs)
         self.cur: Cursor = self.conn.cursor()
@@ -38,10 +39,29 @@ class MoodleDB():
             userid {int} -- The user's ID in to moodle database
 
         Returns:
-            dict -- a dictionary containing `id`, `name`, `surname` 
+            dict -- a dictionary containing `id`, `name`, `surname`
                     and `email` fields
         """
-        return {}
+        return self._get_fileds_from_table('mdl_user', 'id', userid, 'id', 'firstname', 'lastname', 'email')
 
     def get_instanceid_to_username_map(self):
         pass
+
+    def close(self):
+        self.conn.commit()
+        self.close_no_save()
+
+    def close_no_save(self):
+        self.cur.close()
+        self.conn.close()
+
+    def _get_fileds_from_table(self, from_: str, where: str = None, by: str = None, *args) -> dict:
+        self.cur.execute(
+            f"SELECT {str.join('',(f'{f},' for f in args))[:-1]} FROM {self.db}.{from_} "
+            f"WHERE {where}={by};" if where else ';'
+        )
+        res = self.cur.fetchall()
+        if where:
+            return dict(zip(args, res[0]))
+        else:
+            raise NotImplementedError("Can't return a list of objects yet.")
